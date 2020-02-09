52f14d341494f352c9d2330c860ad7cb65b4b3a6
aljaber131 <60335077+aljaber131@users.noreply.github.com>
2020-02-09 10:06:14 -0800

    Writing a Java to find the largest number from three given numbers

---
+++ max.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class max {
+
+	public static void main(String[] args) {
+		Scanner t=new Scanner(System.in);
+		System.out.println("give first number");
+		double d = t.nextDouble();
+		System.out.println("give your 2nd number");
+		double e = t.nextDouble();
+		System.out.println("give your 3rd number");
+		double f = t.nextDouble();
+		double g = maximum(d,e,f);
+		System.out.print(g);
+		
+		
+
+	}
+	public static double maximum(double a, double b, double c) {
+		double max = a;
+		if(max>b) {
+			max=a;
+			
+		}
+		else {
+			max=b;
+		}
+		if(max>c) {
+			return max;
+		}
+		else {
+			max = c;
+			return max;
+		}
+	}
+}
+
+
